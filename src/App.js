e84e4c248f2e35dc3aacb85cc6c1aa1d9a87542a
borikanes <ooludemi@terpmail.umd.edu>
2018-04-22 01:30:15 -0400

    started navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './HomePage.js';
+import BoriNavbar from './BoriNavbar.js';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+            <header className="App-header">
+                < BoriNavbar />
+            </header>
             <Route exact path="/" component={HomePage}/>
         </div>
       </Router>
