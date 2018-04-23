3b125cadeca6c165cef302da6d415265fc53fd0f
borikanes <ooludemi@terpmail.umd.edu>
2018-04-23 00:47:06 -0400

    almost done with the top div in home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,15 +5,12 @@ export default class HomePage extends Component {
     render() {
         return (
             <div>
-                {/* <header className="Home-header">
-                    <div className="Home-header-name-div">
-                        <h1>Oluwabori (Bori) Oludemi</h1>
+                <div className="Home-top-div">
+                    <div className="Home-avatar-div">
                     </div>
-                    <div>
-                        <a href="/" className="Header-nav-buttons-first">HOME</a>
-                    </div>
-                </header> */}
-                <h1>Welcome to borikanes.me</h1>
+                    <h1 style={{'marginBottom': 0}} >Oluwabori Oludemi</h1>
+                    <h2 style={{'marginTop': 0}} >Full stack engineer. Blah blah blah blah blah blah blah blah</h2>
+                </div>
             </div>
         );
     }
