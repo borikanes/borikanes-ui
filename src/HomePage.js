02f62bd7aa3901d11de8266035ce382e341db77d
borikanes <ooludemi@terpmail.umd.edu>
2018-05-02 06:45:41 -0400

    committing in order to save

--- src/HomePage.js
+++ src/HomePage.js
@@ -8,8 +8,7 @@ export default class HomePage extends Component {
                 <div className="Home-top-div">
                     <div className="Home-avatar-div">
                     </div>
-                    <h1 style={{'marginBottom': 0}} >Oluwabori Oludemi</h1>
-                    <h2 style={{'marginTop': 0}} >Full stack engineer. Blah blah blah blah blah blah blah blah</h2>
+                    <p>Hello my name is Oluwabori Oludemi(Bori). Currently a fullstack engineer at Capital One. I love to code and I'm very passionate about learning and teaching different types of technologies.</p>
                 </div>
             </div>
         );
