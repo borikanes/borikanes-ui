e9ef6bd61fe30d3bbf4cfbfb57dcbd286760c595
borikanes <ooludemi@terpmail.umd.edu>
2018-05-06 22:59:53 -0400

    updated homepage content

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,9 +6,9 @@ export default class HomePage extends Component {
         return (
             <div>
                 <div className="Home-top-div">
-                    <div className="Home-avatar-div">
-                    </div>
-                    <p>Hello my name is Oluwabori Oludemi(Bori). Currently a fullstack engineer at Capital One. I love to code and I'm very passionate about learning and teaching different types of technologies.</p>
+                    <div className="Home-avatar-div"></div>
+                    <h1>Oluwabori (Bori) Oludemi</h1>
+                    <p>Currently pledging my allegiance to Capital One. Full stack engineer, I love to code and very passionate about learning and teaching different types of technologies.</p>
                 </div>
             </div>
         );
