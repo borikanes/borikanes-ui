c19bba21ad552878fdcc958ec71bdd14a83d2501
borikanes <ooludemi@terpmail.umd.edu>
2018-05-11 01:34:49 -0400

    started carousel....am i really doing a carousel?

--- src/HomePage.js
+++ src/HomePage.js
@@ -10,6 +10,12 @@ export default class HomePage extends Component {
                     <h1>Oluwabori (Bori) Oludemi</h1>
                     <p>Currently pledging my allegiance to Capital One. Full stack engineer, I love to code and very passionate about learning and teaching different types of technologies.</p>
                 </div>
+                <div className="Home-projects-div">
+                    <h1>Projects</h1>
+                    <div className="Home-carousel-div" >
+
+                    </div>
+                </div>
             </div>
         );
     }
