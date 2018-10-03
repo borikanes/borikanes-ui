fb5d982ccad236b4116204cdb00295d54c2265aa
borikanes <ooludemi@terpmail.umd.edu>
2018-10-02 23:54:05 -0400

    added profile picture to top part of the home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './HomePage.css';
+import profile_picture from './images/profile.png';
 
 export default class HomePage extends Component {
     render() {
         return (
             <div>
                 <div className="Home-top-div">
-                    <div className="Home-avatar-div"></div>
+                    <div className="Home-avatar-div" style={{'backgroundImage': `url(${profile_picture})`}}></div>
                     <h1>Oluwabori (Bori) Oludemi</h1>
                     <p>Currently pledging my allegiance to Capital One. Full stack engineer, I love to code and very passionate about learning and teaching different types of technologies.</p>
                 </div>
