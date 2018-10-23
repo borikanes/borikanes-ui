2ed4c6b5acbab9fefd629239abb9baeb62c0444e
borikanes <ooludemi@terpmail.umd.edu>
2018-10-23 01:59:40 -0400

    added links to projects

--- src/ProjectGrid.js
+++ src/ProjectGrid.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
 import './ProjectGrid.css';
-import profile_picture from './images/profile.png';
 import wedding_shot from './images/wedding-shot.jpg';
 import nessie_shot from './images/nessie-shot.jpg';
 import wedding_ios_shot from './images/wedding-ios-shot.jpg';
 import posed from 'react-pose';
 
 export default class ProjectGrid extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         const Box = posed.div({
             hoverable: true,
@@ -26,13 +21,13 @@ export default class ProjectGrid extends Component {
         return(
             <div className="Project-grid-base-div">
                 <Box className="Project-item-div" style={{'backgroundImage': `url(${wedding_ios_shot})`}}>
-                    <ProjectOverlay title="My Wedding iOS app" subtitle="Swift, iOS"/>
+                    <ProjectOverlay title="My Wedding iOS app" subtitle="Swift, iOS" idvalue="0" />
                 </Box>
                 <Box className="Project-item-div" style={{'backgroundImage': `url(${nessie_shot})`}}>
-                    <ProjectOverlay title="Nessie" subtitle="NodeJS, Python" />
+                    <ProjectOverlay title="Nessie" subtitle="NodeJS, Python" idvalue="1" />
                 </Box>
                 <Box className="Project-item-div" style={{'backgroundImage': `url(${wedding_shot})`}}>
-                    <ProjectOverlay  title="My wedding website" subtitle="React, AWS" />
+                    <ProjectOverlay  title="My wedding website" subtitle="React, AWS" idvalue="2" />
                 </Box>
             </div>
         );
@@ -61,6 +56,19 @@ class ProjectOverlay extends Component {
         })
     }
 
+    handleButtonClick = (event) => {
+        console.log(event);
+        
+        if (event.target.value === "0") {
+            window.open('https://github.com/borikanes/BoriAndMalloryWeddingIOS')
+        } else if (event.target.value === "1") {
+            // Will cloudfront like this?
+            window.open('http://api.reimaginebanking.com/')
+        } else if (event.target.value === "2") {
+            window.open('https://boriandmallory.com/')
+        }
+    }
+
     render() {
         var styles = {
             overlay: {
@@ -76,7 +84,7 @@ class ProjectOverlay extends Component {
                     <p className="Project-grid-top-sub-paragraph">{this.props.subtitle}</p>
                 </div>
                 <div className="Project-grid-bottom-div">
-                    <button className="button button1">LEARN MORE</button>
+                    <button className="button button1" value={this.props.idvalue} onClick={this.handleButtonClick}>LEARN MORE</button>
                 </div>
             </div>
         );
